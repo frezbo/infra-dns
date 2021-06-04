60ff00fc2cde2c4412b1295bf739adbbce663eb5
Noel Georgi <git@frezbo.dev>
2021-06-04 20:34:29 +0530

    Upgrade to Pulumi SDK v3
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-cloudflare/sdk/v2/go/cloudflare"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi-cloudflare/sdk/v3/go/cloudflare"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
